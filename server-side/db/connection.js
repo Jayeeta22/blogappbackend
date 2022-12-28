8c2a752d2f9e7f65da5a502cf434b858fe02ea56
Jayeeta22 <117907901+Jayeeta22@users.noreply.github.com>
2022-12-28 20:57:43 +0530

    changed

--- server-side/db/connection.js
+++ server-side/db/connection.js
@@ -2,8 +2,8 @@ const mongooes=require("mongoose")
 // const dotenv=require("dotenv")
 // connect mongdb Atlus in nodejs
 // dotenv.config({path:"./config.env"})
-const DB=process.env.DATABASE
-mongooes.connect(DB).then(()=>{
+// const DB=process.env.DATABASE
+mongooes.connect("mongodb+srv://jayeetablog1:jayeetablog1@cluster0.fzwdwwr.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log("connection successful")
 }).catch((err)=>{
     console.log(err)
